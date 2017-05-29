7d1486b0349989cf7ada7bb0eeb76b3676ab43f7
Mark Watson <Mark Watson>
2017-05-29 18:54:46 -0500

    Updated webfonts package to include all versions of Quadraat.

--- public/fonts/MyFontsWebfontsKit.js
+++ public/fonts/MyFontsWebfontsKit.js
@@ -1,6 +1,6 @@
 /*
 
- MyFonts Webfont Build ID 2911690, 2014-10-29T09:37:30-0400
+ MyFonts Webfont Build ID 3397708, 2017-05-29T12:40:34-0400
 
  The fonts listed in this notice are subject to the End User License
  Agreement(s) entered into by the website owner. All other parties are 
@@ -9,41 +9,42 @@
  You may obtain a valid license at the URLs below.
 
  Webfont: FF Quadraat Web DemiBold by FontFont
- URL: http://www.myfonts.com/fonts/fontfont/ff-quadraat/ot-demi-bold/
+ URL: https://www.myfonts.com/fonts/fontfont/ff-quadraat/ot-demi-bold/
  Copyright: 2011 Fred Smeijers published by FSI FontShop International GmbH
 
  Webfont: FF Quadraat Web Bold Italic by FontFont
- URL: http://www.myfonts.com/fonts/fontfont/ff-quadraat/ot-bold-italic/
+ URL: https://www.myfonts.com/fonts/fontfont/ff-quadraat/ot-bold-italic/
  Copyright: 2009 Fred Smeijers published by FSI FontShop International GmbH
 
  Webfont: FF Quadraat Web Bold by FontFont
- URL: http://www.myfonts.com/fonts/fontfont/ff-quadraat/ot-bold/
+ URL: https://www.myfonts.com/fonts/fontfont/ff-quadraat/ot-bold/
  Copyright: 2009 Fred Smeijers published by FSI FontShop International GmbH
 
  Webfont: FF Quadraat Web DemiBold Italic by FontFont
- URL: http://www.myfonts.com/fonts/fontfont/ff-quadraat/ot-demi-bold-italic/
+ URL: https://www.myfonts.com/fonts/fontfont/ff-quadraat/ot-demi-bold-italic/
  Copyright: 2011 Fred Smeijers published by FSI FontShop International GmbH
 
  Webfont: FF Quadraat Web Italic by FontFont
- URL: http://www.myfonts.com/fonts/fontfont/ff-quadraat/ot-italic/
+ URL: https://www.myfonts.com/fonts/fontfont/ff-quadraat/ot-italic/
  Copyright: 2009 Fred Smeijers published by FSI FontShop International GmbH
 
  Webfont: FF Quadraat Web by FontFont
- URL: http://www.myfonts.com/fonts/fontfont/ff-quadraat/ot-regular/
+ URL: https://www.myfonts.com/fonts/fontfont/ff-quadraat/ot-regular/
  Copyright: 2009 Fred Smeijers published by FSI FontShop International GmbH
 
 
- License: http://www.myfonts.com/viewlicense?type=web&buildid=2911690
+ License: https://www.myfonts.com/viewlicense?type=web&buildid=3397708
  Licensed pageviews: 500,000
 
- ? 2014 MyFonts Inc
+ ? 2017 MyFonts Inc
 */
-var browserName,browserVersion,webfontType;if("undefined"==typeof woffEnabled)var woffEnabled=!0;var svgEnabled=0,woff2Enabled=1;if("undefined"!=typeof customPath)var path=customPath;else{var scripts=document.getElementsByTagName("SCRIPT"),script=scripts[scripts.length-1].src;script.match("://")||"/"==script.charAt(0)||(script="./"+script);path=script.replace(/\\/g,"/").replace(/\/[^\/]*\/?$/,"")}
-var wfpath=path+"/webfonts/",browsers=[{regex:"MSIE (\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:9,type:"woff"},{version:5,type:"eot"}]},{regex:"Trident/(\\d+\\.\\d+); (.+)?rv:(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$3)",type:[{version:11,type:"woff"}]},{regex:"Firefox[/s](\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:3.6,type:"woff"},{version:3.5,type:"ttf"}]},{regex:"Chrome/(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:36,type:"woff2"},
-{version:6,type:"woff"},{version:4,type:"ttf"}]},{regex:"Mozilla.*Android (\\d+\\.\\d+).*AppleWebKit.*Safari",versionRegex:"new Number(RegExp.$1)",type:[{version:4.1,type:"woff"},{version:3.1,type:"svg#wf"},{version:2.2,type:"ttf"}]},{regex:"Mozilla.*(iPhone|iPad).* OS (\\d+)_(\\d+).* AppleWebKit.*Safari",versionRegex:"new Number(RegExp.$2) + (new Number(RegExp.$3) / 10)",unhinted:!0,type:[{version:5,type:"woff"},{version:4.2,type:"ttf"},{version:1,type:"svg#wf"}]},{regex:"Mozilla.*(iPhone|iPad|BlackBerry).*AppleWebKit.*Safari",
-versionRegex:"1.0",type:[{version:1,type:"svg#wf"}]},{regex:"Version/(\\d+\\.\\d+)(\\.\\d+)? Safari/(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:5.1,type:"woff"},{version:3.1,type:"ttf"}]},{regex:"Opera/(\\d+\\.\\d+)(.+)Version/(\\d+\\.\\d+)(\\.\\d+)?",versionRegex:"new Number(RegExp.$3)",type:[{version:24,type:"woff2"},{version:11.1,type:"woff"},{version:10.1,type:"ttf"}]}],browLen=browsers.length,suffix="",i=0;
-a:for(;i<browLen;i++){var regex=RegExp(browsers[i].regex);if(regex.test(navigator.userAgent)){browserVersion=eval(browsers[i].versionRegex);var typeLen=browsers[i].type.length;for(j=0;j<typeLen;j++)if(browserVersion>=browsers[i].type[j].version&&(!0==browsers[i].unhinted&&(suffix="_unhinted"),webfontType=browsers[i].type[j].type,"woff"!=webfontType||woffEnabled)&&("woff2"!=webfontType||woff2Enabled)&&("svg#wf"!=webfontType||svgEnabled))break a}else webfontType="woff"}
+var protocol=document.location.protocol;"https:"!=protocol&&(protocol="http:");var count=document.createElement("script");count.type="text/javascript";count.async=!0;count.src=protocol+"//hello.myfonts.net/count/33d84c";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(count,s);var browserName,browserVersion,webfontType;if("undefined"==typeof woffEnabled)var woffEnabled=!0;var svgEnabled=0,woff2Enabled=1;
+if("undefined"!=typeof customPath)var path=customPath;else{var scripts=document.getElementsByTagName("SCRIPT"),script=scripts[scripts.length-1].src;script.match("://")||"/"==script.charAt(0)||(script="./"+script);path=script.replace(/\\/g,"/").replace(/\/[^\/]*\/?$/,"")}
+var wfpath=path+"/webfonts/",browsers=[{regex:"MSIE (\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:9,type:"woff"},{version:5,type:"eot"}]},{regex:"Trident/(\\d+\\.\\d+); (.+)?rv:(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$3)",type:[{version:11,type:"woff"}]},{regex:"Firefox[/s](\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:3.6,type:"woff"},{version:3.5,type:"ttf"}]},{regex:"Edge/(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:12,type:"woff"}]},
+{regex:"Chrome/(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:36,type:"woff2"},{version:6,type:"woff"},{version:4,type:"ttf"}]},{regex:"Mozilla.*Android (\\d+\\.\\d+).*AppleWebKit.*Safari",versionRegex:"new Number(RegExp.$1)",type:[{version:4.1,type:"woff"},{version:3.1,type:"svg#wf"},{version:2.2,type:"ttf"}]},{regex:"Mozilla.*(iPhone|iPad).* OS (\\d+)_(\\d+).* AppleWebKit.*Safari",versionRegex:"new Number(RegExp.$2) + (new Number(RegExp.$3) / 10)",unhinted:!0,type:[{version:5,
+type:"woff"},{version:4.2,type:"ttf"},{version:1,type:"svg#wf"}]},{regex:"Mozilla.*(iPhone|iPad|BlackBerry).*AppleWebKit.*Safari",versionRegex:"1.0",type:[{version:1,type:"svg#wf"}]},{regex:"Version/(\\d+\\.\\d+)(\\.\\d+)? Safari/(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:5.1,type:"woff"},{version:3.1,type:"ttf"}]},{regex:"Opera/(\\d+\\.\\d+)(.+)Version/(\\d+\\.\\d+)(\\.\\d+)?",versionRegex:"new Number(RegExp.$3)",type:[{version:24,type:"woff2"},{version:11.1,type:"woff"},
+{version:10.1,type:"ttf"}]}],browLen=browsers.length,suffix="",i=0;
+a:for(;i<browLen;i++){var regex=new RegExp(browsers[i].regex);if(regex.test(navigator.userAgent)){browserVersion=eval(browsers[i].versionRegex);var typeLen=browsers[i].type.length;for(j=0;j<typeLen;j++)if(browserVersion>=browsers[i].type[j].version&&(1==browsers[i].unhinted&&(suffix="_unhinted"),webfontType=browsers[i].type[j].type,"woff"!=webfontType||woffEnabled)&&("woff2"!=webfontType||woff2Enabled)&&("svg#wf"!=webfontType||svgEnabled))break a}else webfontType="woff"}
 /(Macintosh|Android)/.test(navigator.userAgent)&&"svg#wf"!=webfontType&&(suffix="_unhinted");var head=document.getElementsByTagName("head")[0],stylesheet=document.createElement("style");stylesheet.setAttribute("type","text/css");head.appendChild(stylesheet);
-for(var fonts=[{fontFamily:"FFQuadraatWebDemiBold",url:wfpath+"2C6DCA_0"+suffix+"_0."+webfontType},{fontFamily:"FFQuadraat",fontWeight:"bold",fontStyle:"italic",url:wfpath+"2C6DCA_1"+suffix+"_0."+webfontType},{fontFamily:"FFQuadraat",fontWeight:"bold",fontStyle:"normal",url:wfpath+"2C6DCA_2"+suffix+"_0."+webfontType},{fontFamily:"FFQuadraatWebDemiBoldItalic",url:wfpath+"2C6DCA_3"+suffix+"_0."+webfontType},{fontFamily:"FFQuadraat",fontWeight:"normal",fontStyle:"italic",url:wfpath+"2C6DCA_4"+suffix+
-"_0."+webfontType},{fontFamily:"FFQuadraatWeb",url:wfpath+"2C6DCA_5"+suffix+"_0."+webfontType}],len=fonts.length,css="",i=0;i<len;i++){var format="svg#wf"==webfontType?'format("svg")':"ttf"==webfontType?'format("truetype")':"eot"==webfontType?"":'format("'+webfontType+'")',css=css+("@font-face{font-family: "+fonts[i].fontFamily+";src:url("+fonts[i].url+")"+format+";");fonts[i].fontWeight&&(css+="font-weight: "+fonts[i].fontWeight+";");fonts[i].fontStyle&&(css+="font-style: "+fonts[i].fontStyle+";");
-css+="}"}stylesheet.styleSheet?stylesheet.styleSheet.cssText=css:stylesheet.innerHTML=css;
+for(var fonts=[{fontFamily:"FFQuadraatWebDemiBold",url:wfpath+"33D84C_0"+suffix+"_0."+webfontType},{fontFamily:"FFQuadraatWebBoldItalic",url:wfpath+"33D84C_1"+suffix+"_0."+webfontType},{fontFamily:"FFQuadraatWebBold",url:wfpath+"33D84C_2"+suffix+"_0."+webfontType},{fontFamily:"FFQuadraatWebDemiBoldItalic",url:wfpath+"33D84C_3"+suffix+"_0."+webfontType},{fontFamily:"FFQuadraatWebItalic",url:wfpath+"33D84C_4"+suffix+"_0."+webfontType},{fontFamily:"FFQuadraatWeb",url:wfpath+"33D84C_5"+suffix+"_0."+webfontType}],
+len=fonts.length,css="",i=0;i<len;i++){var format="svg#wf"==webfontType?'format("svg")':"ttf"==webfontType?'format("truetype")':"eot"==webfontType?"":'format("'+webfontType+'")',css=css+("@font-face{font-family: "+fonts[i].fontFamily+";src:url("+fonts[i].url+")"+format+";");fonts[i].fontWeight&&(css+="font-weight: "+fonts[i].fontWeight+";");fonts[i].fontStyle&&(css+="font-style: "+fonts[i].fontStyle+";");css+="}"}stylesheet.styleSheet?stylesheet.styleSheet.cssText=css:stylesheet.innerHTML=css;
