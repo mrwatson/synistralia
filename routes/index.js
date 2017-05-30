2a90896b7020b6030f6554da1541b42c45d799ec
Mark Watson <Mark Watson>
2017-05-29 20:30:12 -0500

    Started building out resume page.

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Synistralia Software, LLC' });
 });
 
+router.get('/mark-resume', function(req, res) {
+  res.render('mark-resume', { title: 'Synistralia Software, LLC | Resume for Mark Watson' });
+});
+
 module.exports = router;
